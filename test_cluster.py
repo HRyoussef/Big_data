0e65ead13b02088d91527be63ec4b184a50b6077
Global Name <hirchaou.youssef@gmail.com>
2026-04-20 09:32:30 +0200

    Déployer et utiliser un cluster Spark Standalone

---
+++ test_cluster.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+import time
+
+spark = SparkSession.builder \
+    .appName("TaxiCluster") \
+    .master("spark://PYTHON-08:7077") \
+    .getOrCreate()
+
+print("Master :", spark.sparkContext.master)
+
+start = time.time()
+df = spark.read.parquet("/home/youssef.hirchaou@Digital-Grenoble.local/campus/Big_data_architecture_distribuées/yellow_tripdata_2026-01.parquet")
+print("Nombre de lignes :", df.count())
+end = time.time()
+
+print(f"Temps cluster : {end - start:.2f} secondes")
+spark.stop()
